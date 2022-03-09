8748eef33650b514b97d476cfe247b4661a8feab
shasherazi <hassanrandomz@gmail.com>
2022-03-09 21:41:01 +0500

    updated functions

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def create_server_connection(host_name, user_name, user_password):
+def create_db_connection(host_name, user_name, user_password, db_name):
     connection = None
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
-            passwd=user_password
+            passwd=user_password,
+            database=db_name
         )
         print("MySQL Database connection successful")
     except Error as err:
@@ -21,15 +22,16 @@ def create_server_connection(host_name, user_name, user_password):
     return connection
 
 
-def create_database(connection, query):
+def execute_query(connection, query):
     cursor = connection.cursor()
     try:
         cursor.execute(query)
-        print("Database created successfully")
+        connection.commit()
+        print("Query successful")
     except Error as err:
         print(f"Error: '{err}'")
 
 
-connection = create_server_connection("localhost", "root", os.getenv("pw"))
-createDatabaseQuery = "CREATE DATABASE test"
-create_database(connection, createDatabaseQuery)
+connection = create_db_connection("localhost", "root", os.getenv("pw"), "test")
+createDatabaseQuery = "CREATE DATABASE IF NOT EXISTS test"
+execute_query(connection, createDatabaseQuery)
