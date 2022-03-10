a8942c3b81144f48db85fe3721e7e3d9da3908cd
shasherazi <hassanrandomz@gmail.com>
2022-03-11 01:12:05 +0500

    Added user related functions

--- db.py
+++ db.py
@@ -12,22 +12,38 @@ def createDbConnection(db_name):
     return connection
 
 
+connection = createDbConnection("users.db")
+
+
 def executeQuery(connection, query):
     cursor = connection.cursor()
     try:
         cursor.execute(query)
         connection.commit()
-        print("Query successful")
+        return True
     except sqlite3.Error as err:
         print(f"Error: '{err}'")
+        return False
 
 
-connection = createDbConnection("users.db")
-createDatabaseQuery = """
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL
-);
-"""
-executeQuery(connection, createDatabaseQuery)
+def checkIfUserExists(username):
+    query = f"SELECT * FROM users WHERE username='{username}'"
+    cursor = connection.cursor()
+    cursor.execute(query)
+    if cursor.fetchone():
+        return True
+    return False
+
+
+def createUser(username, password):
+    query = f"INSERT INTO users (username, password) VALUES ('{username}', '{password}')"
+    executeQuery(connection, query)
+
+
+def clearDb():
+    """this will permanently delete the database, use with caution"""
+
+    query = "DELETE FROM users"
+    executeQuery(connection, query)
+    query = "DELETE FROM sqlite_sequence WHERE name='users'"
+    executeQuery(connection, query)
