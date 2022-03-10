748437a7cc349a1240a1fd7a2bd25b35da33e23f
shasherazi <hassanrandomz@gmail.com>
2022-03-11 01:12:27 +0500

    Implemented signup

--- user.py
+++ user.py
@@ -1,15 +1,24 @@
 import db
+import hashlib
 
 
 class User:
     def __init__(self, username, password):
         self.username = username
-        self.password = password
+        self.password = hashlib.sha256(
+            password.encode()).hexdigest()  # hashed password
 
     def signup(self):
-        print("Signing up")
-        # TODO: Implement signup and save to database
+        if(db.checkIfUserExists(self.username)):
+            print("User already exists")
+        else:
+            db.createUser(self.username, self.password)
+            print("User created")
 
     def login(self):
         print("Logging in")
         # TODO: Implement login and check against database
+
+
+user = User("joe", "mama")
+user.signup()
